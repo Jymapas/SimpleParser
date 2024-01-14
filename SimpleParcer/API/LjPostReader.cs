69a38ac8b16980d42a98598b61e211d6624dc133
Jymapas <jymapas@yandex.ru>
2024-01-14 23:47:24 +0600

    feat(PostReader): #2: rebase LjPostReader

--- SimpleParcer/API/LjPostReader.cs
+++ SimpleParcer/API/LjPostReader.cs
@@ -1,7 +1,46 @@
-﻿namespace SimpleParser.API
+﻿using HtmlAgilityPack;
+using SimpleParser.Constants;
+using System.Web;
+
+namespace SimpleParser.API
 {
     internal class LjPostReader
     {
+        public async Task<string> GetAnnounce()
+        {
+            var html = await new HttpClient().GetStringAsync(Paths.PostUri);
 
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            var postContent = doc.DocumentNode.SelectSingleNode("//article[contains(@class, 'b-singlepost-body')]");
+            if (postContent != null)
+            {
+                var contentHtml = postContent.InnerHtml;
+                var firstBreakIndex = contentHtml.IndexOf("<br><br>");
+                if (firstBreakIndex != -1)
+                {
+                    contentHtml = contentHtml[(firstBreakIndex + 8)..]; // +8 to remove the first "<br><br>"
+                }
+
+                postContent.InnerHtml = contentHtml;
+
+                foreach (var link in postContent.SelectNodes(".//a[@href]"))
+                {
+                    var href = link.GetAttributeValue("href", string.Empty);
+                    if (!href.StartsWith("https://www.livejournal.com/away?to="))
+                        continue;
+                    href = href.Replace("https://www.livejournal.com/away?to=", "");
+                    href = HttpUtility.UrlDecode(href); // Normalize encoded characters
+                    link.SetAttributeValue("href", href);
+                }
+
+                return postContent.InnerHtml;
+            }
+            else
+            {
+                return "Post content not found.";
+            }
+        }
     }
 }
--- SimpleParcer/Program.cs
+++ SimpleParcer/Program.cs
@@ -1,47 +1,10 @@
-﻿using System.Runtime.Serialization;
-using HtmlAgilityPack;
-using System.Web;
-using SimpleParser.Constants;
-
-namespace SimpleParser
+﻿namespace SimpleParser
 {
     internal class Program
     {
         private async Task Main(string[] args)
         {
-            var html = await new HttpClient().GetStringAsync(Paths.PostUri);
-
-            var doc = new HtmlDocument();
-            doc.LoadHtml(html);
-
-            var postContent = doc.DocumentNode.SelectSingleNode("//article[contains(@class, 'b-singlepost-body')]");
-            if (postContent != null)
-            {
-                var contentHtml = postContent.InnerHtml;
-                var firstBreakIndex = contentHtml.IndexOf("<br><br>");
-                if (firstBreakIndex != -1)
-                {
-                    contentHtml = contentHtml[(firstBreakIndex + 8)..]; // +8 to remove the first "<br><br>"
-                }
-
-                postContent.InnerHtml = contentHtml;
-
-                foreach (var link in postContent.SelectNodes(".//a[@href]"))
-                {
-                    var href = link.GetAttributeValue("href", string.Empty);
-                    if (!href.StartsWith("https://www.livejournal.com/away?to=")) 
-                        continue;
-                    href = href.Replace("https://www.livejournal.com/away?to=", "");
-                    href = HttpUtility.UrlDecode(href); // Normalize encoded characters
-                    link.SetAttributeValue("href", href);
-                }
-
-                Console.WriteLine(postContent.InnerHtml);
-            }
-            else
-            {
-                Console.WriteLine("Post content not found.");
-            }
+            
         }
 
     }
