2a8d55b0717be3f700f8f9a8391fa946054f4664
Jymapas <jymapas@yandex.ru>
2024-01-15 23:48:51 +0600

    feat(bot): #2: add MessageHandler base logic

---
+++ SimpleParcer/API/MessagesHandler.cs
@@ -0,0 +1,28 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using SimpleParser.Constants;
+
+namespace SimpleParser.API
+{
+    internal class MessagesHandler
+    {
+        private ITelegramBotClient _botClient;
+        private CancellationToken _cancellationToken;
+
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
+            CancellationToken cancellationToken)
+        {
+            _botClient = botClient;
+            _cancellationToken = cancellationToken;
+            if ((update.Type != UpdateType.Message) || (update.Message!.Type != MessageType.Text)) return;
+            var message = update?.Message;
+            var messageText = message.Text.Trim().ToLower();
+
+            if (!messageText.Equals(Commands.Announcement))
+            {
+                return;
+            }
+        }
+    }
+}
--- SimpleParcer/Constants/ServiceLines.cs
+++ SimpleParcer/Constants/ServiceLines.cs
@@ -4,6 +4,7 @@
     {
         internal const string TgHead = "Продолжаем вести список синхронов в Санкт-Петербурге.";
         internal const string ReceivingPostError = "Post content not found.";
+        internal const string UnknownCommand = "Unknown command.";
         internal const string RemovableString = "https://www.livejournal.com/away?to=";
     }
 }
