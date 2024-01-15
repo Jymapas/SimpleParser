ba35aa79bb6e83a4bfb0a573064aec8987688602
Jymapas <jymapas@yandex.ru>
2024-01-15 22:46:23 +0600

    feat(consts): #2: add BotToken path

--- SimpleParcer/Constants/Paths.cs
+++ SimpleParcer/Constants/Paths.cs
@@ -3,5 +3,6 @@
     internal class Paths
     {
         internal const string PostUri = @"https://chgk-spb.livejournal.com/2596838.html";
+        internal const string BotToken = "TgToken.txt";
     }
 }
