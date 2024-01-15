9a2b8caf2815ad572563669bb55fb2e93d6da206
Jymapas <jymapas@yandex.ru>
2024-01-15 22:43:47 +0600

    feat(reader): #2: create txt-reader

---
+++ SimpleParcer/Helpers/ReadHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleParser.Helpers
+{
+    internal class ReadHelper
+    {
+        /// <summary>
+        /// Возвращает весь текст .txt-файла в string
+        /// </summary>
+        /// <param name="path">Путь к файлу</param>
+        /// <returns>Текст файла как string</returns>
+        public static string GetFromTxt(string path)
+        {
+            using var sr = new FileInfo(path).OpenText();
+            return sr.ReadToEnd();
+        }
+    }
+}
