e59e539b067d659c185a4e1af9437e2c5c3af880
Jymapas <jymapas@yandex.ru>
2024-01-15 23:34:47 +0600

    feat(bot): #2: add bot connection

--- SimpleParcer/API/Connect.cs
+++ SimpleParcer/API/Connect.cs
@@ -1,7 +1,28 @@
-﻿namespace SimpleParser.API
+﻿using SimpleParser.Helpers;
+using SimpleParser.Constants;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+
+namespace SimpleParser.API
 {
     internal class Connect
     {
         private string _botToken;
+
+        internal void Start()
+        {
+            _botToken = ReadHelper.GetFromTxt(Paths.BotToken);
+
+            if (_botToken.Equals(string.Empty))
+                return;
+
+            ITelegramBotClient bot = new TelegramBotClient(_botToken);
+            using CancellationTokenSource cts = new();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { }
+            };
+        }
     }
 }
