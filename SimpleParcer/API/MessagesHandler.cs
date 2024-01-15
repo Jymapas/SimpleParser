88467981e829319a73d6f61f9350f847d4340797
Jymapas <jymapas@yandex.ru>
2024-01-16 00:19:11 +0600

    feat(bot): #2: add seniding post logic

--- SimpleParcer/API/MessagesHandler.cs
+++ SimpleParcer/API/MessagesHandler.cs
@@ -18,11 +18,39 @@ namespace SimpleParser.API
             if ((update.Type != UpdateType.Message) || (update.Message!.Type != MessageType.Text)) return;
             var message = update?.Message;
             var messageText = message.Text.Trim().ToLower();
+            var chatId = message.Chat.Id;
 
             if (!messageText.Equals(Commands.Announcement))
             {
+                await SendMessage(chatId, ServiceLines.UnknownCommand);
                 return;
             }
+
+            var reader = new LjPostReader();
+            var announceSource = await reader.GetAnnounce();
+            
+            var announce = announceSource == ServiceLines.ReceivingPostError ?
+                announceSource :
+                ServiceLines.TgHead + announceSource;
+
+            await SendMessage(chatId, announce);
+            return;
+        }
+
+        /// <summary>
+        /// Отправка сообщений ботом
+        /// </summary>
+        /// <param name="id">Id пользователя, чата или канала, куда будет отправлено сообщение</param>
+        /// <param name="text">Текст сообщения в HTML формате</param>
+        private async Task SendMessage(ChatId id, string text)
+        {
+            await _botClient.SendTextMessageAsync(
+                chatId: id,
+                text: text,
+                parseMode: ParseMode.Html,
+                disableWebPagePreview: true,
+                cancellationToken: _cancellationToken
+            );
         }
     }
 }
