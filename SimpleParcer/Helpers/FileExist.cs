89282360e7dfc0bd87ffbeda19c9553ed9595702
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2024-07-18 23:28:40 +0500

    feat(Helpers): #4: create CheckExistence method

--- SimpleParcer/Helpers/FileExist.cs
+++ SimpleParcer/Helpers/FileExist.cs
@@ -1,5 +1,17 @@
-﻿namespace SimpleParser.Helpers;
+﻿using SimpleParser.Constants;
+
+namespace SimpleParser.Helpers;
 
 public class FileExist
 {
+    public static bool CheckExistence()
+    {
+        if (File.Exists(Paths.BotToken))
+        {
+            return true;
+        }
+
+        Console.WriteLine("File with BotToken doesn't exist!");
+        return false;
+    }
 }
