808efe69acb58b7b7ca977545e2bd4047ce1db8d
Jymapas <jymapas@yandex.ru>
2024-10-02 19:30:15 +0500

    test: #4: add testes for LjPostReader

--- SimpleParser.Tests/LjPostReaderTests.cs
+++ SimpleParser.Tests/LjPostReaderTests.cs
@@ -1,11 +1,72 @@
+using Moq;
+using Moq.Protected;
+using SimpleParser.API;
+using SimpleParser.Constants;
+
 namespace SimpleParser.Tests
 {
     public class LjPostReaderTests
     {
         [Fact]
-        public void PostReaderTest()
+        public async Task GetAnnounceAsync_ReturnsFormattedAnnouncement_WhenPostExists()
         {
+            // Arrange
+            var html = "<article class='b-singlepost-body'><p><b>03 окт€бр€ (чт)</b>Some announcement content</p></article>";
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
 
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = System.Net.HttpStatusCode.OK,
+                    Content = new StringContent(html),
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var ljPostReader = new LjPostReader(DateTime.Parse("2024-10-03"));
+            typeof(LjPostReader)
+                .GetField("httpClient", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .SetValue(ljPostReader, httpClient);
+
+            // Act
+            var result = await ljPostReader.GetAnnounceAsync();
+
+            // Assert
+            Assert.Contains("Some announcement content", result);
+        }
+
+        [Fact]
+        public async Task GetAnnounceAsync_ReturnsError_WhenPostNotFound()
+        {
+            // Arrange
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = System.Net.HttpStatusCode.NotFound,
+                });
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+            var ljPostReader = new LjPostReader();
+            typeof(LjPostReader)
+                .GetField("httpClient", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+                .SetValue(ljPostReader, httpClient);
+
+            // Act
+            var result = await ljPostReader.GetAnnounceAsync();
+
+            // Assert
+            Assert.Equal(ServiceLines.ReceivingPostError, result);
         }
     }
 }
