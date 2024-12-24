52106ac89b30434fde4c9d27281f950f73a9ab7e
Aleksandr Petrakov <apetrakov@tourmalinecore.com>
2024-12-25 01:09:30 +0500

    feat(Connect): add bot commands set

--- SimpleParcer/API/Connect.cs
+++ SimpleParcer/API/Connect.cs
@@ -1,20 +1,20 @@
-﻿using Microsoft.Extensions.Configuration;
-using SimpleParser.Helpers;
-using SimpleParser.Constants;
+﻿using SimpleParser.Constants;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
 
 namespace SimpleParser.API
 {
     internal class Connect
     {
-        private string _botToken;
         private readonly MessagesHandler _messagesHandler = new();
+        private string _botToken;
+        private CancellationToken _cancellationToken;
 
         internal async Task Start()
         {
             _botToken = Environment.GetEnvironmentVariable("BOT_TOKEN") ?? string.Empty;
-            
+
             if (_botToken.Equals(string.Empty))
             {
                 Console.WriteLine(ServiceLines.TgTokenError);
@@ -23,20 +23,43 @@ namespace SimpleParser.API
 
             ITelegramBotClient bot = new TelegramBotClient(_botToken);
             using CancellationTokenSource cts = new();
-            var cancellationToken = cts.Token;
-            var receiverOptions = new ReceiverOptions
-            {
-                AllowedUpdates = { }
-            };
+            _cancellationToken = cts.Token;
+            
+            await SetBotCommands(bot);
+            
+            var receiverOptions = new ReceiverOptions();
 
             bot.StartReceiving(
                 _messagesHandler.HandleUpdateAsync,
                 _messagesHandler.HandleErrorAsync,
                 receiverOptions,
-                cancellationToken
+                _cancellationToken
             );
-            
-            await Task.Delay(Timeout.Infinite, cancellationToken);
+
+            await Task.Delay(Timeout.Infinite, _cancellationToken);
+        }
+
+        private async Task SetBotCommands(ITelegramBotClient bot)
+        {
+            var commands = new[]
+            {
+                new BotCommand
+                {
+                    Command = Commands.Announcement,
+                    Description = Commands.AnnouncementDescription,
+                },
+                new BotCommand
+                {
+                    Command = Commands.Recent,
+                    Description = Commands.RecentDescription,
+                },
+            };
+
+            await bot.SetMyCommands(
+                commands,
+                BotCommandScope.AllPrivateChats(),
+                cancellationToken: _cancellationToken
+            );
         }
     }
 }
--- SimpleParcer/Constants/Commands.cs
+++ SimpleParcer/Constants/Commands.cs
@@ -3,6 +3,9 @@
     internal class Commands
     {
         internal const string Announcement = "/announcement";
+        internal const string AnnouncementDescription = "Получить список актуальных турниров";
+
         internal const string Recent = "/recent";
+        internal const string RecentDescription = "Получить пост с последнего дня публикации";
     }
 }
